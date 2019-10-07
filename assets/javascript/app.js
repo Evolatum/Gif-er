37a5c6780627b550f459c4fb609cf4f2f79f2086
Santiago MVO <santiago.mvao@gmail.com>
2019-10-07 13:16:02 -0500

    added functionality to add GIFs to favorite section, where you can display all saved GIFs and remove from section, which is stored in localStorage

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -17,6 +17,12 @@ var gifr = {
 
     //Holds current topic for when querying more of same topic
     currentTopic:"",
+
+    //Array of favorite gifs
+    favorites:[],
+
+    //Holds if browser supports local storage
+    localStor:false,
     
     //Holds current offset for when quering more of same topic
     offset:0,
@@ -24,6 +30,7 @@ var gifr = {
     //Generates button for every topic in array
     genBtns:function(){
         $("#btnsHere").empty();
+        $("#btnsHere").append(`<button class="btn btn-primary" id="favoritesBtn">Favories</button>`);
         for(let topic of this.topics){
           $("#btnsHere").append(`<button class="btn btn-info" id=${topic.split(" ").join("+")}>${topic}</button>`);
         }
@@ -63,7 +70,6 @@ var gifr = {
 
     //Displays requested GIFs
     addGifs:function(gifs){
-        console.log(gifs.data);
         for(let gif of gifs.data){
             $("#gifsHere").append(`
             <div class="col-xl-3 col-lg-4 col-md-4 col-sm-6 col-12">
@@ -74,6 +80,7 @@ var gifr = {
                         <p class="card-text"><b>Rating:</b> ${gif.rating}</p>
                         <p class="card-text"><b>Uploded:</b> ${gif.import_datetime}</p>
                         <p class="card-text"><b>By:</b> `+(gif.username===""?"Anonymus":`<a href="${gif.user.profile_url}" target="_blank"><img src="${gif.user.avatar_url}" class="profilePic"/>${gif.user.display_name}</a>`)+`</p> 
+                        <button class="btn btn-primary favGif" id="fav${gif.id}">Add to Favorites</button>
                     </div>
                 </div>
             </div>`);
@@ -91,9 +98,88 @@ var gifr = {
             $(`#${id}`).attr("data-still","true")
         }
     },
+
+    //Checks if browser supports local storage
+    checkLocalStorage:function(){
+        if (typeof(Storage) !== "undefined") {
+            this.localStor=true;
+        } else {
+            console.log("No web storage Support");
+        }
+    },
+
+    //Adds a GIF to the favorites array
+    addFav:function(favBtn){
+        var newFav = favBtn.substring(3);
+        if(this.checkFav(newFav)){
+            this.favorites.push(newFav);
+            if(this.localStor){
+                window.localStorage.setItem('favs',this.favorites.join(","));
+            }
+        }
+        $(`#${favBtn}`).remove();
+    },
+
+    //Checks if new favorite is already in array
+    checkFav(newFav){
+        for(fav of this.favorites){
+            if(fav===newFav)return false;
+        }
+
+        return true;
+    },
+
+    //Displays favorites
+    displayFavs:function(){
+        //Retrieves local storage of favorites
+        if(this.localStor){
+            this.favorites = window.localStorage.getItem('favs').split(",");;
+        }
+
+        $.ajax({
+            url: `https://api.giphy.com/v1/gifs?api_key=${y+x+this.z}&ids=${this.favorites.join(",")}`,
+            method: "GET"
+        }).then(function(response) {
+            $("#gifsHere").empty();
+            for(let gif of response.data){
+                $("#gifsHere").append(`
+                <div class="col-xl-3 col-lg-4 col-md-4 col-sm-6 col-12">
+                    <div class="card bg-light border-info">
+                        <img src="${gifr.giphySRC1}${gif.id}/giphy_s.gif" id=${gif.id} class="card-img-top" alt="gif" data-still="true">
+                        <div class="card-body">
+                            <h5 class="card-title text-info">${gif.title===""||gif.title===" "?gif.slug.split("-").join(" "):gif.title}</h5>
+                            <p class="card-text"><b>Rating:</b> ${gif.rating}</p>
+                            <p class="card-text"><b>Uploded:</b> ${gif.import_datetime}</p>
+                            <p class="card-text"><b>By:</b> `+(gif.username===""?"Anonymus":`<a href="${gif.user.profile_url}" target="_blank"><img src="${gif.user.avatar_url}" class="profilePic"/>${gif.user.display_name}</a>`)+`</p> 
+                            <button class="btn btn-danger" id="remFav${gif.id}">Remove favorite</button>
+                        </div>
+                    </div>
+                </div>`);
+            }
+        });
+
+    },
+
+    //Removes a GIF from favories
+    delFav:function(oldFav){
+        for(let fav of this.favorites){
+            if(fav===oldFav.substring(6)){
+                this.favorites=this.favorites.filter(function(value,index,arr){
+                    return value !== fav;
+                });
+            }
+        }
+        $(`#${oldFav}`).parent().parent().parent().remove();
+        if(this.localStor){
+            window.localStorage.setItem('favs',this.favorites.join(","));
+        }
+    },
 }
 
 $(document).ready(function() {
+    //Checks if browser supports local storage
+    gifr.checkLocalStorage();
+
     //Initial buttons display
     gifr.genBtns();
 
@@ -123,5 +209,20 @@ $(document).ready(function() {
         $(".btn-success").remove();
         gifr.queryTopic(gifr.currentTopic);
     });
-    
+
+    //Add GIF to favorites
+    $(document).on("click", ".favGif", function() {
+        gifr.addFav($(this).attr("id").toString());
+    });
+
+    //Display favorite GIFs
+    $(document).on("click", "#favoritesBtn", function() {
+        gifr.currentTopic="";
+        gifr.displayFavs();
+    });
+
+    //Removes a GIF from favories
+    $(document).on("click", ".btn-danger", function() {
+        gifr.delFav($(this).attr("id").toString());
+    });
 });
